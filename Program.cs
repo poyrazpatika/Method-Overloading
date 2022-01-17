ea4484c50a1b606bd0325fb047e17a589058c6e4
yppyrz <yppyrz@gmail.com>
2022-01-17 23:08:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out paramatreler
+            string sayi = "999";
+            int outSayi;
+
+            bool sonuc = int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Sonuç başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            // Metot Aşırı Yükleme - Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Zikriye","Ürkmez");
+
+            // metot imzası
+            // metotAdı + parametre sayısı + parametre
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
